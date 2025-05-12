150f6b83d6b24d223434ac5cf274cc3895410708
chanyukai <chanyukai88@gmail.com>
2025-05-12 21:13:07 +0800

    implement Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Grid, GridItem, Text } from "@chakra-ui/react"
+import Navbar from "@/components/Navbar"
 
 const App = () => {
   return (
-    <div>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
+    >
+      <GridItem area="nav">
+        <Navbar />
+      </GridItem>
+      <GridItem area="aside">
+        <Text>Aside</Text>
+      </GridItem>
+      <GridItem area="main">
+        <Text>Main</Text>
+      </GridItem>
+    </Grid>
   )
 }
 
