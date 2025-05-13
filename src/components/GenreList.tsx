2a1120223299320fc62137051992020416b1fb0d
chanyukai <chanyukai88@gmail.com>
2025-05-13 22:51:49 +0800

    clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 import useGenres, { type Genre } from "@/hooks/useGenres"
-import { HStack, List, Image, Spinner, Link } from "@chakra-ui/react"
+import { HStack, List, Image, Spinner, Link, Heading } from "@chakra-ui/react"
 
 interface Props {
   onSelectedGenre: (genre: Genre) => void,
@@ -10,23 +10,27 @@ const GenreList = ({ selectedGenre ,onSelectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   return (
-    <List.Root>
-      {isLoading && <Spinner />}
-      {!isLoading && data.map(genre => (
-        <List.Item key={genre.id} paddingY={5} listStyle={"none"}>
-          <HStack>
-            <Image
-              src={genre.image_background}
-              alt={genre.name}
-              boxSize="32px"
-              borderRadius="md"
-              objectFit="cover"
-            />
-            <Link fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectedGenre(genre)} fontSize="lg" colorPalette="white">{genre.name}</Link>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading as="h2" fontSize="2xl" marginY={5}>Genres</Heading>
+      <List.Root>
+        {isLoading && <Spinner />}
+        {!isLoading && data.map(genre => (
+          <List.Item key={genre.id} paddingY={5} listStyle={"none"}>
+            <HStack>
+              <Image
+                src={genre.image_background}
+                alt={genre.name}
+                boxSize="32px"
+                borderRadius="md"
+                objectFit="cover"
+              />
+              <Link fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectedGenre(genre)} fontSize="lg" colorPalette="white">{genre.name}</Link>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
+    
   )
 }
 
