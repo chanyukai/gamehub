a9e9b9ac6c40da5984bc9e6bce89df5d9deef4ce
chanyukai <chanyukai88@gmail.com>
2025-05-17 15:25:04 +0800

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import { Provider } from "@/components/ui/provider"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import router from './routes'
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Provider>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <RouterProvider router={router} />
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </Provider>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,22 @@
+import GameGrid from "@/components/GameGrid"
+import GameHeading from "@/components/GameHeading"
+import GenreList from "@/components/GenreList"
+import PlatformSelector from "@/components/PlatformSelector"
+import SortSelector from "@/components/SortSelector"
 import { Grid, GridItem, HStack } from "@chakra-ui/react"
-import Navbar from "@/components/Navbar"
-import GameGrid from "./components/GameGrid"
-import GenreList from "./components/GenreList"
-import PlatformSelector from "./components/PlatformSelector"
-import SortSelector from "./components/SortSelector"
-import GameHeading from "./components/GameHeading"
 
-const App = () => {
-  
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: '1fr',
         lg: '200px 1fr'
       }}
     >
-      <GridItem area="nav" paddingX={5}>
-        <Navbar />
-      </GridItem>
       <GridItem area="aside" paddingX={5}>
         {/* <GenreList onSelectedGenre={(genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre} /> */}
         <GenreList /> 
@@ -41,4 +36,4 @@ const App = () => {
   )
 }
 
-export default App
+export default HomePage
