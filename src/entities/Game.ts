c5ab4d6ae758f7f55dbb4bdcf289a44d4a83f36d
chanyukai <chanyukai88@gmail.com>
2025-05-17 16:38:05 +0800

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import type { Genre } from "./Genre";
 import type { Platform } from "./Platform";
+import type { Publisher } from "./Publisher";
 
 
 export interface Game {
@@ -7,6 +9,8 @@ export interface Game {
   slug: string;
   background_image: string;
   description_raw: string;
+  genres: Genre[],
+  publishers: Publisher[],
   parent_platforms: [
     { platform: Platform; }
   ];
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,5 @@
 import ExpandableText from "@/components/ExpandableText";
+import GameAttributes from "@/components/GameAttributes";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router"
@@ -15,8 +16,9 @@ const GameDetailPage = () => {
     <>
       <Heading marginBottom={3}>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   )
 }
 
-export default GameDetailPage
+export default GameDetailPage;
